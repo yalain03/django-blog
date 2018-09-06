f09b43b415ce95258f99b46d0863cbfedf246779
Yves <yalain9@gmail.com>
2018-09-06 15:41:32 -0500

    Access to db

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Yves',
-        'title': '1st post',
-        'content': 'This is the first post on the blog',
-        'date_posted': 'September 5, 2018'
-    },
-    {
-        'author': 'Yves',
-        'title': '1 more',
-        'content': 'Another dummy post to pass content',
-        'date_posted': 'September 5, 2018'
-    }
-]
-
 def home(request):
-    context = { 'posts': posts }
+    context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about(request):
