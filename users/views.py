d2a4a968c0f964a8e1c0a150801bbae96f705e96
Yves <yalain9@gmail.com>
2018-09-07 12:22:47 -0500

    Logou route

--- users/views.py
+++ users/views.py
@@ -11,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account created. Now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
