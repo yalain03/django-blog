418181d202990b2c5fd75354dab1be58d75dd8a8
Yves <yalain9@gmail.com>
2018-09-08 11:35:38 -0500

    User profile + profile pic

--- users/views.py
+++ users/views.py
@@ -10,7 +10,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created. Now able to login')
             return redirect('login')
     else:
