625daf4ba299e909c38356999ae3efb82889c6fa
Yves <yalain9@gmail.com>
2018-09-11 10:17:30 -0500

    Post create + update

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # Create your views here.
@@ -6,6 +7,23 @@ from .models import Post
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
